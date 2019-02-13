1e9cb76ea20bcd53131c527f02846c396855dded
acmeinke <acmeinke@oakland.edu>
2019-02-12 20:13:11 -0500

    added method Ninety_Nine_Bottles_of_Beer() functionality

--- Main.java
+++ Main.java
@@ -7,6 +7,21 @@ public class Main {
 
         }
     public void Ninety_Nine_Bottles_of_Beer(){
-
+                for (int beerCount=99; beerCount>-1;)
+        {
+            System.out.print(beerCount + " bottles of beer on the wall, ");
+            System.out.println(beerCount + " bottles of beer, ");
+            if (beerCount==0)
+            {
+                System.out.print("Go to the store, buy some more, ");
+                System.out.println("99 bottles of beer on the wall.\n");
+                System.exit(0);
+            }
+            else
+                System.out.print("Take one down, pass it around, ");
+            String s;
+            s = (--beerCount == 1)?"":"s";
+            System.out.println(beerCount + " bottles of beer on the wall.\n");
+        }
     }
 }
