e787448bb5545eeebb215f94bce76493c1f7b034
acmeinke <acmeinke@oakland.edu>
2019-02-12 20:06:25 -0500

    Added Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+	// write your code here
+        new Main().Ninety_Nine_Bottles_of_Beer();
+
+        }
+    public void Ninety_Nine_Bottles_of_Beer(){
+
+    }
+}
